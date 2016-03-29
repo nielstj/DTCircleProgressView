02f4050555363c185dd875be63d27a6c43e7c802
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2016-03-29 16:00:40 +0800

    - Carthage integration -

---
+++ Example/DTCircleProgressView/DTCircleProgressView.h
@@ -0,0 +1,19 @@
+//
+//  DTCircleProgressView.h
+//  DTCircleProgressView
+//
+//  Created by Daniel Tjuatja on 29/3/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTCircleProgressView.
+FOUNDATION_EXPORT double DTCircleProgressViewVersionNumber;
+
+//! Project version string for DTCircleProgressView.
+FOUNDATION_EXPORT const unsigned char DTCircleProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTCircleProgressView/PublicHeader.h>
+
+
